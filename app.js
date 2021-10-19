520077df6109984f19f1f45627e466de75274155
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-10-18 22:00:12 -0300

    Resoluções 5,6 e 7

--- app.js
+++ app.js
@@ -41,11 +41,11 @@ function retornaMaior(a,b){
 
 //FizzBuzz
 /* 
-Divisível por 3 retorna "Fizz"
-Divisível por 5 retorna "Buzz"
-Divisível por 3 e 5 retorna "FizzBuzz"
-Não divisível por 3 ou 5 retorna Entrada
-Não é um número retorna "Não é um número"
+    Divisível por 3 retorna "Fizz"
+    Divisível por 5 retorna "Buzz"
+    Divisível por 3 e 5 retorna "FizzBuzz"
+    Não divisível por 3 ou 5 retorna Entrada
+    Não é um número retorna "Não é um número"
 */ 
 function fizzBuzz(a){
     if(typeof(a) !== "number"){
@@ -64,10 +64,10 @@ function fizzBuzz(a){
 
 //Medidor de velocidade
 /*
-velocidade Máxima: 70km
-a cada 5km acima do limite vc ganha 1 ponto na carteira
-Math.Floor() arredonda número
-caso pontos maior que 12 -> "Carteira suspendida"
+    velocidade Máxima: 70km
+    a cada 5km acima do limite vc ganha 1 ponto na carteira
+    Math.Floor() arredonda número
+    caso pontos maior que 12 -> "Carteira suspendida"
 */
 const velocidadeMáxima = 70;
 function medidor(velocidade){if(velocidade <= 70 ) return "Okay";
@@ -77,3 +77,84 @@ function medidor(velocidade){if(velocidade <= 70 ) return "Okay";
         else return ("Você levou " + pontos + " Pontos na carteira.");
     }else return "Valor incoerente ou nulo";}
 //Fim Medidor de velocidade
+
+//Impar ou Par
+/*
+    Receber uma quantidade de valores para avaliar 
+    função exibe se cada valor é par ou impar 
+*/ 
+function imparPar(valor){
+    for (i = 0 ;i <= valor; i++){
+        if((i%2) === 0){
+           console.log(i+" 'PAR'");
+        }else{
+            console.log(i+" 'IMPAR'");
+        }
+    }
+}
+
+function imparOuPar(type,value){
+    if(type == "impar"){
+        for(i=0; i <= value; i++){
+            if(i%2 != 0){
+                console.log(i," IMPAR");
+            }
+        }
+    }else if(type == "par"){
+        for(i=0; i <= value; i++){
+            if(i%2 === 0){
+                console.log(i," PAR");
+            }
+        }
+    }else return "oq??";
+}
+//Fim Impar ou Par
+
+//Encontre o String
+/*
+    Criar um método para ler propriedades de um objeto e
+    exibir somente as propriedades do tipo string que estão neste objeto
+*/
+const filmes = {
+    titulo: "Os mequetrfes",
+    duracao: 230,
+    qualidadeVideo: "zuada",
+    qualidadeAudio: "aceitável",
+    nota: 6.5,
+    critica: "Filme agradável"
+} 
+function retornaString (obj){
+    for(itens in obj){
+        if(typeof(obj[itens]) === "string"){
+            console.log(itens+": "+obj[itens]);
+        }
+    }
+}
+//Fim encontre o String
+
+//multiplos de 3 e 5
+/*
+    Criar função somar que retorna a soma de todos os múltiplos de 3 e 5
+    m = n * k
+*/
+function somaMultiplos(m){
+    const n3 = 3;
+    const n5 = 5;
+    let contadorN = 0;
+    for(i = 0; i <= m; i++){
+        for(k = 0;k <= m; k++){
+            if(((n3*k) <= m)){
+                if(i === (n3 * k)){
+                    contadorN += i;
+                }
+                if(i === (n5 * k)) {
+                    contadorN += i;
+                }
+            }else break;
+        }
+    }
+    console.log("A soma dos multiplos de 3 e 5 é de: " +contadorN);
+}
+//Fim multiplos de 3 e 5
+
+
